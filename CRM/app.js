5cd84752d71ca5944ed95580519744e020ddc8b7
chankyle <kyle.chan@live.com>
2019-04-25 21:20:01 -0700

    Base components to connect CoreUI and to connect MongoDB

--- CRM/app.js
+++ CRM/app.js
@@ -4,10 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var bodyParser = require('body-parser');
+//const mongoose = require("mongoose");
+//let db = mongoose.connect('mongodb://localhost/ToTheMoonDB');
+
+
+var monk = require('monk');
+var db = monk('localhost:27017/CRM');
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var dataEntryRouter = require('./routes/data-entry');
+var customerEntryRouter = require('./routes/customer-entry');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,19 +29,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+
+//handle routers to various page
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/data-entry', dataEntryRouter);
+app.use('/customer-entry', customerEntryRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
 
   // render the error page
   res.status(err.status || 500);
