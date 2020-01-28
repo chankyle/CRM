99f442255100d0181898c8b72fca092aa8399656
chankyle <kyle.chan@live.com>
2020-01-27 20:22:59 -0800

    Added Registration Code - For Hekoru test

--- CRM/routes/index.js
+++ CRM/routes/index.js
@@ -32,15 +32,17 @@ router.get('/register', function(req, res) {
 
 /* Register User */
 router.post('/register', function(req, res) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-        if (err) {
-            return res.render('register', { account : account });
-        }
+    if (req.body.regCode == "KPCRS"){
+        Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+            if (err) {
+                return res.render('register', { account : account });
+            }
 
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
+            passport.authenticate('local')(req, res, function () {
+                res.redirect('/');
+            });
         });
-    });
+    }
 });
 
 /* Reroute after Logout Successful. */
